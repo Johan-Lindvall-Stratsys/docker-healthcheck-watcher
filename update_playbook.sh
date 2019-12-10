86eca6cc3c4a989c4efa6b24eb6ab90cef4a5494
Johan Lindvall <johan.lindvall@stratsys.se>
2019-12-10 12:38:09 +0100

    Removed redundant argument

--- update_playbook.sh
+++ update_playbook.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 SERVICE=healthcheck-watcher:watcher
 
-echo docker run --rm -v "$(pwd)/stack:/playbook/stack" -w /playbook "$STRATSYS_CR_URL/stratsys-envhandler:latest" --branch dev --destination dev --localSource /playbook --token GITHUB_PLAYBOOK_TOKEN --url "$GITHUB_PLAYBOOK_URL" --email "$GITHUB_PLAYBOOK_EMAIL" --author "$GITHUB_PLAYBOOK_AUTHOR" --verbosity 4 copy "$SERVICE"
-docker run --rm -v "$(pwd)/stack:/playbook/stack" -w /playbook "$STRATSYS_CR_URL/stratsys-envhandler:latest" --branch dev --destination dev --localSource /playbook --token "$GITHUB_PLAYBOOK_TOKEN" --url "$GITHUB_PLAYBOOK_URL" --email "$GITHUB_PLAYBOOK_EMAIL" --author "$GITHUB_PLAYBOOK_AUTHOR" --verbosity 4 copy "$SERVICE"
+docker run --rm -v "$(pwd)/stack:/playbook/stack" -w /playbook "$STRATSYS_CR_URL/stratsys-envhandler:latest" --destination dev --localSource /playbook --token "$GITHUB_PLAYBOOK_TOKEN" --url "$GITHUB_PLAYBOOK_URL" --email "$GITHUB_PLAYBOOK_EMAIL" --author "$GITHUB_PLAYBOOK_AUTHOR" --verbosity 4 copy "$SERVICE"
