8a4e3d30ea80c4a568d88e1ac82bff9bc493e366
Johan Lindvall <johan.lindvall@stratsys.se>
2019-12-10 10:08:27 +0100

    Fixed bad script

--- update_playbook.sh
+++ update_playbook.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-docker run --rm \
--v ./stack:/playbook/stack \
--w /playbook \
-"$STRATSYS_CR_URL/stratsys-envhandler:latest" \ 
---branch dev \
---destination dev \
---localSource /playbook \
---token "$GITHUB_PLAYBOOK_TOKEN" \
---url "$GITHUB_PLAYBOOK_URL" \
---email "$GITHUB_PLAYBOOK_EMAIL" \
---author "$GITHUB_PLAYBOOK_AUTHOR" \
---verbosity 4 \
-copy healthcheck-watcher:watcher
+SERVICE=healthcheck-watcher:watcher
+
+docker run --rm -v "$(pwd)/stack:/playbook/stack" -w /playbook "$STRATSYS_CR_URL/stratsys-envhandler:latest" --branch dev --destination dev --localSource /playbook --token "$GITHUB_PLAYBOOK_TOKEN" --url "$GITHUB_PLAYBOOK_URL" --email "$GITHUB_PLAYBOOK_EMAIL" --author "$GITHUB_PLAYBOOK_AUTHOR" --verbosity 4 copy "$SERVICE"
