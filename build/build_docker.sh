ba336190b9e902846186c76cf051e05532ace448
Johan Lindvall <johan.lindvall@stratsys.se>
2019-12-11 10:31:51 +0100

    Fix multi-tagging

--- build/build_docker.sh
+++ build/build_docker.sh
@@ -3,4 +3,4 @@ source build/common.sh
 set -x
 LATEST_TAG_ARG=""
 [[ "$PUSH_LATEST" == "1" ]] && LATEST_TAG_ARG="-t "$IMAGE_TAG_LATEST""
-docker build --rm -t "$IMAGE_TAG" "$LATEST_TAG_ARG" .
+docker build --rm -t "$IMAGE_TAG" $LATEST_TAG_ARG .
--- build/remove_dockerimage.sh
+++ build/remove_dockerimage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source build/common.sh
 set -x
-docker rmi "$IMAGE_TAG" "$IMAGE_TAG_LATEST"
+docker rmi "$IMAGE_TAG" $IMAGE_TAG_LATEST
